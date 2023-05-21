291958f0bdba11d8f7f72666021b16d3dba9c460
OskarPawłowski <oskar7799@gmail.com>
2023-05-21 20:57:28 +0200

    Nowy feature

---
+++ tescik/tescik/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tescik
+{
+    internal class Class1
+    {
+        void comment()
+        {
+            Console.WriteLine("Przykładowa klasa");
+        }
+    }
+}
